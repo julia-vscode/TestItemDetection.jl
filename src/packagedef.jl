c92d7d11b1b25d46efcdf7b0867c729713f52aa1
Jacob Quinn <quinn.jacobd@gmail.com>
2023-01-12 16:30:59 -0700

    Parse setup keyword arg for testitems to include a shared test setup module

--- src/packagedef.jl
+++ src/packagedef.jl
@@ -26,6 +26,7 @@ function find_test_items_detail!(node, testitems, errors)
         else
             option_tags = nothing
             option_default_imports = nothing
+            option_setup = nothing
 
             # Now check our keyword args
             for i in child_nodes[3:end-1]
@@ -67,6 +68,26 @@ function find_test_items_detail!(node, testitems, errors)
                     end
 
                     option_default_imports = parse(Bool, CSTParser.valof(i.args[2]))
+                elseif CSTParser.valof(i.args[1])=="setup"
+                    if option_setup!==nothing
+                        push!(errors, (error="The keyword argument setup cannot be specified more than once.", range=range))
+                        return
+                    end
+
+                    if !(i.args[2].head == :vect)
+                        push!(errors, (error="The keyword argument `setup` only accepts a vector of `@testsetup` names.", range=range))
+                        return
+                    end
+                    option_setup = Symbol[]
+
+                    for j in i.args[2].args
+                        if !(j isa EXPR && j.head==:IDENTIFIER)
+                            push!(errors, (error="The keyword argument `setup` only accepts a vector of `@testsetup` names.", range=range))
+                            return
+                        end
+
+                        push!(option_setup, Symbol(CSTParser.valof(j.args[1])))
+                    end
                 else
                     push!(errors, (error="Unknown keyword argument.", range=range))
                     return
@@ -81,13 +102,17 @@ function find_test_items_detail!(node, testitems, errors)
                 option_default_imports = true
             end
 
+            if option_setup===nothing
+                option_setup = Symbol[]
+            end
+
             # TODO + 1 here is from the space before the begin end block. We might have to detect that,
             # not sure whether that is always assigned to the begin end block EXPR
             code_pos = get_file_loc(child_nodes[end])[2] + 1 + length("begin")
 
             code_range = code_pos:code_pos+child_nodes[end].span - 1 - length("begin") - length("end")
 
-            push!(testitems, (name=CSTParser.valof(node.args[3]), range=range, code_range=code_range, option_default_imports=option_default_imports, option_tags=option_tags))
+            push!(testitems, (name=CSTParser.valof(node.args[3]), range=range, code_range=code_range, option_default_imports=option_default_imports, option_tags=option_tags, option_setup=option_setup))
         end
     elseif node.head == :module && length(node.args)>=3 && node.args[3] isa EXPR && node.args[3].head==:block
         for i in node.args[3].args
